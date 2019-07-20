5494ccb643b234c8065ad71a4baf8f0321bd5a04
Carson Howard <cjhoward92@gmail.com>
2019-07-19 17:26:53 -0700

    Mid work on key validation

--- rustdblib/src/key.rs
+++ rustdblib/src/key.rs
@@ -1,5 +1,8 @@
 use std::collections::hash_map::DefaultHasher;
+use std::fmt::{self, Result as FmtResult};
 use std::hash::{Hash, Hasher};
+
+const MAX_KEY_LENGTH: usize = 255;
 
 #[derive(Hash)]
 pub struct Key {
@@ -12,6 +15,35 @@ impl Key {
       key: String::from(key),
     }
   }
+}
+
+#[derive(Debug)]
+pub enum KeyError {
+  NullKey,
+  BlankKey,
+  ExceedsMaxLength(usize),
+}
+
+impl fmt::Display for KeyError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> FmtResult {
+    match self {
+      KeyError::NullKey => write!(f, "{:?} - key cannot be null", self),
+      KeyError::BlankKey => write!(f, "{:?} - key cannot be blank", self),
+      KeyError::ExceedsMaxLength(max) => write!(f, "{:?} - key cannot exceed max length of {}", self, max),
+    }
+  }
+}
+
+fn validate_key(key: &Key) -> Result<bool, KeyError> {
+  let key_slice = &key.key[..];
+  let key_len = key_slice.trim().len();
+  if key_len == 0 {
+    return Err(KeyError::BlankKey)
+  } else if key_len > MAX_KEY_LENGTH {
+    return Err(KeyError::ExceedsMaxLength(MAX_KEY_LENGTH))
+  }
+
+  Ok(true)
 }
 
 pub fn hash_key(key: &Key) -> u64 {
